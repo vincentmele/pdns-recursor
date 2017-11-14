b142028946507ae49cbb25e70ee9e45a1e5a1191
Vince Mele <vmele@inoc.com>
2017-11-14 15:19:03 -0500

    removed requirement for bash.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:latest
 
 MAINTAINER Vince Mele <vmele@inoc.com>
 
-RUN apk --no-cache add pdns-recursor bash
+RUN apk --no-cache add pdns-recursor
 COPY files/entrypoint.sh /entrypoint.sh
 
 EXPOSE 5353 8083
--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ALLOW_FROM=${ALLOW_FROM:-127.0.0.1/8}
 LOCAL_PORT=${LOCAL_PORT:-5353}
@@ -8,19 +8,19 @@ API_READONLY=${API_READONLY:-yes}
 API_KEY=${API_KEY:-"readonly"}
 LOGLEVEL=${LOGLEVEL:-6}
 
-OPTIONS=()
-OPTIONS+="--local-address=${LOCAL_ADDRESS} "
-OPTIONS+="--local-port=${LOCAL_PORT} "
-OPTIONS+="--allow-from=${ALLOW_FROM} "
+OPTIONS=""
+OPTIONS="${OPTIONS}--local-address=${LOCAL_ADDRESS} "
+OPTIONS="${OPTIONS}--local-port=${LOCAL_PORT} "
+OPTIONS="${OPTIONS}--allow-from=${ALLOW_FROM} "
 #OPTIONS+="--setuid=pdns "
 #OPTIONS+="--setgid=pdns "
-OPTIONS+="--webserver=yes "
-OPTIONS+="--webserver-address=0.0.0.0 "
-OPTIONS+="--webserver-port=${WEBSERVER_PORT} "
-OPTIONS+="--api-key=${API_KEY} "
-OPTIONS+="--api-readonly=${API_READONLY} "
-OPTIONS+="--loglevel=${LOGLEVEL} "
-OPTIONS+="--daemon=no "
+OPTIONS="${OPTIONS}--webserver=yes "
+OPTIONS="${OPTIONS}--webserver-address=0.0.0.0 "
+OPTIONS="${OPTIONS}--webserver-port=${WEBSERVER_PORT} "
+OPTIONS="${OPTIONS}--api-key=${API_KEY} "
+OPTIONS="${OPTIONS}--api-readonly=${API_READONLY} "
+OPTIONS="${OPTIONS}--loglevel=${LOGLEVEL} "
+OPTIONS="${OPTIONS}--daemon=no "
 
 # Start PowerDNS
 /usr/sbin/pdns_recursor ${OPTIONS}
